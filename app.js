52dfda81ebd576b62712e43d276db920715ea627
Priyanka-Kewat <priyankakewat43@gmail.com>
2025-08-25 00:20:51 +0530

    Parcel, jsxcomp

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeadComp, { jsxheading } from "./Some";
+
+const heading = React.createElement(
+  "h1",
+  { id: "head" },
+  "Hello world from react js csv file"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
